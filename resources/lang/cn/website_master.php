81e716048191c85657cd0327aa2c94f4a4c77307
sheldonldev <xiadanli0320@gmail.com>
2020-08-18 15:39:38 +0800

    font family

--- resources/lang/cn/website_master.php
+++ resources/lang/cn/website_master.php
@@ -6,6 +6,6 @@ return [
     'category_list' => '分类列表',
     'messages' => '留 言',
     'copyright' => '版权所有',
-    'owner' => '南德其中',
-    'owner_site' => '南德其中主页',
+    'owner' => '北京其中科技发展集团有限公司',
+    'owner_site' => '公司主页',
 ];
